de7869d0882316eb1972b0475be0b8c45e1f7683
Ginetti Pinetti <pinetti@iMac-di-Ginetti.lan>
2021-08-22 10:46:48 +0200

    Jump_Force final with DashMode && intro

--- Assets/Script/ObstacleMovement.cs
+++ Assets/Script/ObstacleMovement.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
-    private PlayerController playerControllerScript;
     public float speed = 25;
+
+    private PlayerController playerControllerScript;
     private float bound = -15;
+    private bool check = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +20,27 @@ public class ObstacleMovement : MonoBehaviour
     void Update()
     {
         if (!playerControllerScript.gameOver)
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-
+        {
+            // move the backgroud and the object
+            if (!playerControllerScript.dashMode)
+                transform.Translate(Vector3.left * Time.deltaTime * speed);
+             // increase the speed if in dash mode
+            else
+                transform.Translate(Vector3.left * Time.deltaTime * speed * 3);
+        }
+        // handle score & destruction of obstacles
         if (transform.position.x < bound && gameObject.CompareTag("Obstacle"))
+        {
             Destroy(gameObject);
+            Debug.Log("Score : " + playerControllerScript.score);
+        }
+        if (transform.position.x < 0 && check)
+        {
+           if (playerControllerScript.dashMode)
+                playerControllerScript.score += 3;
+           else
+                playerControllerScript.score += 1;
+            check = false;
+        }
     }
 }
